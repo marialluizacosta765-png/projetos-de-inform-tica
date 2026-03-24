75ae862b009c682c4f7c3b8da1454afa473240ce
marialluizacosta765-png <marialluizacosta765@gmail.com>
2026-03-24 16:11:12 -0300

    Add files via upload

---
+++ maria luiza 2 info/maria luiza 2 info/projeto-java/Principal.java
@@ -0,0 +1,7 @@
+public class Principal {
+    // This is the "Main Method" the error is asking for
+    public static void main(String[] args) {
+        // Your code goes here!
+        System.out.println("Hello, the program is working!");
+    }
+}
